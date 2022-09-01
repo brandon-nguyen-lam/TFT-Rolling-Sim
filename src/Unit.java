dc12aeb7a11a319d8e863a340d2c30517cfad57b
Brandon Lam <brandonnguyenlam@gmail.com>
2022-08-31 21:13:38 -0500

    Removed star level as it wasn't needed since you can just do 3 of one unit.

--- src/Unit.java
+++ src/Unit.java
@@ -2,11 +2,9 @@ public class Unit {
 
     String name;
     int cost;
-    int star;
 
-    public Unit (String name, int cost, int star){
+    public Unit (String name, int cost){
         this.name = name;
         this.cost = cost;
-        this.star = star;
     }
 }
